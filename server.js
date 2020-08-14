cd68d8eac52b0f3ce0848b425c0aab4094444dba
ringfungi <ringfungi@gmail.com>
2020-08-14 06:59:51 +0100

    Changed server's port

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(express.static("express"));// default URL for website
 app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/express/index.html'));
     //__dirname : It will resolve to your project folder.
-  });const server = http.createServer(app);
-const port = 3000;
-server.listen(port);console.debug('Server listening on port ' + port);
+  });
+const server = http.createServer(app);
+server.listen(process.env.PORT || 5000, '0.0.0.0');
+console.debug('Server listening on port ' + port);
